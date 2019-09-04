93cd36c8bf3240cdc72a44c9e5bddff88968ccc3
Benjamin Gutzmann <gutzmannbenjamin@gmail.com>
2019-09-04 15:39:26 +0200

    Update all

--- app/app.py
+++ app/app.py
@@ -2,31 +2,49 @@ from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS, cross_origin
 import json
 from jsonschema import validate, ValidationError, SchemaError
-from models import db
+from models import db, OptimizationTask
+from pathlib import Path
 # Import of the models
 # https://www.compose.com/articles/using-postgresql-through-sqlalchemy/
 
+# Schema that has to be validated against the uploaded json
 JSON_SCHEMA_UPLOAD = "./json_schema/schema_upload.json"
 
+# Folder for optimization data
+OPTIMIZATION_DATA = "/optimization-data/"
+
+# File ending for json
+JSON_ENDING = ".json"
+
+# Database to our optimization postgres
 DATABASE_URL = "postgresql+psycopg2://root:root@postgres:5432/optimization"
 
+# Create a flask app
 app = Flask(__name__)
+# Configs for flask app
 app.config["DEBUG"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Enable cross origin resource sharing
 CORS(app)
 
+# Add the created app to our database
+# Basically our empty database with its models gets the information to which existing database to connect
 db.init_app(app)
 
+# Now we want to create all our tables/models (here optimization_tasks table)
+# As we need to access the app we use app_context while creating the tables
 with app.app_context():
     db.create_all()
 
-
+# Our main upload page where we put our json
 @app.route("/upload", methods=['GET', 'POST'])
 @cross_origin()
 def upload_file():
+    # This is our most important path
     if request.method == 'POST':
         # https://stackoverflow.com/questions/46136478/flask-upload-how-to-get-file-name
+        # See if there's a file in our selection field
         if not request.files.get('file', None):
             error = "No file selected!"
             return render_template('upload.html', error=error)
@@ -35,13 +53,17 @@ def upload_file():
         file_upload = request.files["file"]
         req_data = json.load(file_upload)
 
+        # Load validation schema form json_schema
         with open(JSON_SCHEMA_UPLOAD, "r") as f:
             schema_upload = json.load(f)
 
         # Check the json against our json schema as provided in ./json_schema/schema_upload.json
         try:
+            # Validate file
             validate(instance=req_data, schema=schema_upload)
+        # In case there's an error with our data
         except ValidationError as e:
+            # A message to show where there's a problem in the json file
             error = {
                 "message": f"Validation failed. {e.message}",
                 "code": e.validator,
@@ -49,11 +71,54 @@ def upload_file():
             }
 
             return render_template('upload.html', error=error)
-
+        # In case there's an error with our schema itself
         except SchemaError as e:
+            # Print error
             error = str(e)
 
             return render_template('upload.html', error=error)
+
+        # Now since no errors appeared the task will be added to the database
+        # First we split our request data into our columns
+
+        # Author, project are not necessary and will be replaced by standards if not given
+        author = req_data.get("author", "Max Mustermann")
+        project = req_data.get("project", "Standardprojekt")
+        # optimization_id, optimization and type are necessary and can be retrieved directly
+        optimization_id = req_data["optimization_id"]
+        type = req_data["type"]
+        optimization = req_data["optimization"]
+        # data is also necessary and can be retrieved directly; data will be written as file, as it's a big chunk
+        # of data and can easily be stored/loaded as json
+        data = req_data["data"]
+
+        # Create instance of task
+        optimizationtask = OptimizationTask(
+                                author=author,
+                                project=project,
+                                optimization_id=optimization_id,
+                                opt_type=type,
+                                optimization=optimization
+                            )
+
+        # Where to store the data
+        filepath = f"{OPTIMIZATION_DATA}{optimization_id}{JSON_ENDING}"
+
+         # Try adding file to folder and adding job to table
+        try:
+            # Open created filepath
+            with open(filepath, "w") as f:
+                # Write json to it
+                json.dump(data, f)
+
+            # With app_context
+            with app.app_context():
+                # Add task to table
+                db.session.add(optimizationtask)
+                # Push it to the server
+                db.session.commit()
+        except Exception:
+            Path(filepath).unlink()
 
         return jsonify(req_data)
 
@@ -64,7 +129,12 @@ def upload_file():
             })
         return render_template('upload.html')
 
-    return render_template('upload.html')
+
+# Our optimization page where we can see the progress of running optimizations
+@app.route("/optimization", methods=["GET"])
+@cross_origin()
+def show_optimizations():
+    pass
 
 
 if __name__ == "__main__":
--- app/models.py
+++ app/models.py
@@ -17,14 +17,6 @@ class BaseModel(db.Model):
             column: value for column, value in self._to_dict().items()
         })
 
-    def json(self):
-        """
-                Define a base way to jsonify models, dealing with datetime objects
-        """
-        return {
-            column: value for column, value in self._to_dict().items()
-        }
-
 
 class OptimizationTask(BaseModel, db.Model):
     __tablename__ = "optimization_tasks"
@@ -32,65 +24,31 @@ class OptimizationTask(BaseModel, db.Model):
     author = db.Column(db.String)
     # Column for project
     project = db.Column(db.String)
-    # Column for calculation_id
-    calculation_id = db.Column(db.String)
-    # Column for model_id
-    model_id = db.Column(db.String)
+    # # Column for calculation_id
+    # calculation_id = db.Column(db.String)
+    # # Column for model_id
+    # model_id = db.Column(db.String)
     # Column for optimization_id
     optimization_id = db.Column(db.String, primary_key=True)
     # Column for type
     type = db.Column(db.String)
     # Column for version
-    version = db.Column(db.String)
+    # version = db.Column(db.String)
     # Column for optimization
     optimization = db.Column(db.PickleType)
     # Column for data
-    data = db.Column(db.PickleType)
+    # data = db.Column(db.PickleType)
 
-    def __init__(self, author, project, calculation_id, model_id, optimization_id, opt_type, version, optimization, data, **args):
+    def __init__(self, author, project, optimization_id, opt_type, optimization, **args):
         super().__init__(**args)
 
         self.author = author
         self.project = project
-        self.calculation_id = calculation_id
-        self.model_id = model_id
+        # self.calculation_id = calculation_id
+        # self.model_id = model_id
         self.optimization_id = optimization_id
         self.type = opt_type
-        self.version = version
+        # self.version = version
         self.optimization = optimization
-        self.data = data
+        # self.data = data
 
-
-class CalculationTask(BaseModel, db.Model):
-    __tablename__ = "calculation_tasks"
-
-    author = db.Column(db.String)
-    # Column for project
-    project = db.Column(db.String)
-    # Column for calculation_id
-    calculation_id = db.Column(db.String)
-    # Column for model_id
-    model_id = db.Column(db.String)
-    # Column for optimization_id
-    optimization_id = db.Column(db.String, primary_key=True)
-    # Column for type
-    type = db.Column(db.String)
-    # Column for version
-    version = db.Column(db.String)
-    # Column for optimization
-    optimization = db.Column(db.PickleType)
-    # Column for data
-    data = db.Column(db.PickleType)
-
-    def __init__(self, author, project, calculation_id, model_id, optimization_id, opt_type, version, optimization, data, **args):
-        super().__init__(**args)
-
-        self.author = author
-        self.project = project
-        self.calculation_id = calculation_id
-        self.model_id = model_id
-        self.optimization_id = optimization_id
-        self.type = opt_type
-        self.version = version
-        self.optimization = optimization
-        self.data = data
